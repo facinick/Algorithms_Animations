5f0673f58c76be429c24953325e6b44650a95db6
facinick <facinick@gmail.com>
2024-05-25 15:50:03 +0530

    initial setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import { PercolatingGrid } from '../../components/PercolatingGrid/PercolatingGrid';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Percolating Grid
+      </header>
+      <main>
+        <PercolatingGrid />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PercolatingGrid/PercolatingGrid.tsx
@@ -0,0 +1,11 @@
+import styles from "./PercolatingGrid.module.css"
+
+export const PercolatingGrid = (): JSX.Element => {
+  return (
+    <>
+    <div className={styles.colorthis}>
+      hello
+    </div>
+    </>
+  )
+}
