250a442f15b72480bce8f62040a3f5f5785700b0
facinick <facinick@gmail.com>
2024-05-28 13:50:32 +0530

    css stuff

--- src/components/PercolatingGrid/Node/GridNode.tsx
+++ src/components/PercolatingGrid/Node/GridNode.tsx
@@ -6,20 +6,25 @@ interface Props {
   parentId: number;
   open: boolean;
   onClick: () => void
+  onMouseEnter?: React.MouseEventHandler<HTMLDivElement> | undefined
+  onMouseLeave?: React.MouseEventHandler<HTMLDivElement> | undefined
 }
 
-export const GridNode = ({id, parentId, open, onClick}: Props): JSX.Element => {
+export const GridNode = ({id, parentId, open, onClick, onMouseEnter, onMouseLeave}: Props): JSX.Element => {
 
   return (
     <>
       <motion.div 
-      onClick={onClick}
-      className={styles.gridnode}
-      style={{
-        backgroundColor: open ? "blue" : "grey"
-      }}
+        onClick={onClick}
+        className={styles.gridnode}
+        layoutId="node"
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        style={{
+          backgroundColor: open ? "blue" : "grey"
+        }}
       >
-        {id}
+          {id}
       </motion.div>
     </>
   )
--- src/components/PercolatingGrid/PercolatingGrid.tsx
+++ src/components/PercolatingGrid/PercolatingGrid.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useEffect, useMemo, useState } from "react";
 import useSound from "use-sound";
 import waterDropSound from '../../assets/sounds/water_drop.mp3';
@@ -18,6 +19,12 @@ export const PercolatingGrid = (): JSX.Element => {
 
   const [percolating, setPercolating] = useState<boolean>(false);
 
+  const percolation = useMemo(() => new Percolation(nRows, nCols), [nRows, nCols])
+
+  const [id, setId] = useState<Array<number>>(() => percolation.getData());
+
+  const [open, setOpen] = useState<Array<boolean>>(() => percolation.getOpenData());
+
   const [playWaterDropSound] = useSound(waterDropSound, {
     sprite: {
       drop1: [400, 1000],
@@ -26,11 +33,8 @@ export const PercolatingGrid = (): JSX.Element => {
     },
   });
 
-  const percolation = useMemo(() =>  new Percolation(nRows, nCols), [nRows, nCols])
-
-  const [id, setId] = useState<Array<number>>(() => percolation.getData());
-
-  const [open, setOpen] = useState<Array<boolean>>(() => percolation.getOpenData());
+  const [hoveredNode, setHoveredNode] = useState<number>(-1);
+  const [hoveredNodeTimer, setHoveredNodeTimer] = useState<NodeJS.Timeout | null>(null);
 
   const NODE_WIDTH = 50;
   const NODE_HEIGHT = 50;
@@ -38,7 +42,7 @@ export const PercolatingGrid = (): JSX.Element => {
   const GRID_HEIGHT = NODE_HEIGHT * nRows;
 
   const onGridNodeClick = (p: number) => {
-    if(!percolation.isOpen(p)) {
+    if (!percolation.isOpen(p)) {
       percolation.openSite(p)
       playWaterDropSound({
         id: "drop1"
@@ -48,10 +52,30 @@ export const PercolatingGrid = (): JSX.Element => {
     }
   }
 
-  useEffect(() =>  {
+  useEffect(() => {
     const isPercolating = percolation.isPercolating()
     setPercolating(isPercolating)
   }, [open, percolation])
+
+  const handleMouseEnter = (index: number) => {
+    // Clear any existing timer
+    if (hoveredNodeTimer) {
+      clearTimeout(hoveredNodeTimer);
+    }
+    // Set a new timer
+    const timer = setTimeout(() => {
+      setHoveredNode(index);
+    }, 50); // Change 200 to the desired delay in milliseconds
+    setHoveredNodeTimer(timer);
+  };
+
+  const handleMouseLeave = () => {
+    // Clear the timer if the mouse leaves before the timer completes
+    if (hoveredNodeTimer) {
+      clearTimeout(hoveredNodeTimer);
+      setHoveredNodeTimer(null);
+    }
+  };
 
   return (
     <>
@@ -64,12 +88,35 @@ export const PercolatingGrid = (): JSX.Element => {
           gridTemplateColumns: `repeat(${nCols}, ${NODE_WIDTH}px)`,
           gridTemplateRows: `repeat(${nRows}, ${NODE_HEIGHT}px)`,
         }}
+        onMouseLeave={() => handleMouseLeave()}
       >
         {
           id.map((node, index, id) => {
             if (index !== 0 && index !== nLength - 1) {
               return (
-                <GridNode key={index} onClick={() => onGridNodeClick(index)} id={index} parentId={id[index]} open={open[index]} />
+                <div
+
+                  style={{ position: "relative" }}>
+                  {hoveredNode === index && (
+                    <motion.div
+                      layoutId="hovered-node"
+                      className={styles['hovered-node']}
+                      style={{
+                        width: NODE_WIDTH,
+                        height: NODE_HEIGHT,
+                      }}
+                    />
+                  )}
+                  <GridNode
+                    key={index}
+                    onClick={() => onGridNodeClick(index)}
+                    id={index}
+                    parentId={id[index]}
+                    open={open[index]}
+                    onMouseEnter={() =>
+                      handleMouseEnter(index)}
+                  />
+                </div>
               )
             } else return null;
           })
